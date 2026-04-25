0af26b9051c5d81e5425740359d9f1e079d72812
autoraven <71033102+autoraven@users.noreply.github.com>
2026-04-26 02:59:24 +0700

    gg and caramolly

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -405,7 +405,7 @@ export default function AdminPanel() {
             </div>
           </div>
         </main>
-        <footer style={{ textAlign:"center",padding:"24px 0 16px",color:"var(--muted)",fontSize:11,opacity:0.4,position:"relative",zIndex:1 }}>Created by GG</footer>
+        <footer style={{ textAlign:"center",padding:"24px 0 16px",color:"var(--muted)",fontSize:11,opacity:0.4,position:"relative",zIndex:1 }}>Created by GG & Caramolly</footer>
       </div>
     </>
   );
